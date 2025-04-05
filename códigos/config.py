d4a01797ee14245c0858850643be4becfe4ea52e
Bruno Miglioretto <brunomiglioretto@gmail.com>
2025-04-05 20:43:07 -0300

    add config and utils scripts

---
+++ códigos/config.py
@@ -0,0 +1,24 @@
+import json
+import logging
+import time
+
+CONFIG_FILE = "./config.json"
+
+
+with open(CONFIG_FILE) as f:
+    content = f.read()
+    config = json.loads(content)
+
+DATASETS_FOLDER = config.get("datasets_folder")
+RESULTS_FOLDER = config.get("results_folder")
+LOGS_FOLDER = config.get("logs_folder")
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    filename=f"{LOGS_FOLDER}/run-{time.time()}.txt",
+    filemode='w',
+    level=logging.DEBUG
+)
---
+++ códigos/utils.py
@@ -0,0 +1,37 @@
+from aeon.datasets import load_classification
+from aeon.datasets import load_from_ts_file
+
+from config import logger
+
+
+def load_dataset(dataset_name, dataset_folder):
+    try:
+        logger.info(f"Carregando {dataset_name}")
+
+        X_train, y_train = load_from_ts_file(f"{dataset_folder}/{dataset_name}/{dataset_name}_TRAIN.ts")
+        X_test, y_test = load_from_ts_file(f"{dataset_folder}/{dataset_name}/{dataset_name}_TEST.ts")
+
+        logger.info("Carregamento finalizado com sucesso")
+
+        return {
+            "X_train": X_train,
+            "y_train": y_train,
+            "X_test": X_test,
+            "y_test": y_test,
+        }
+    except Exception as e:
+        logger.warning(e)
+        logger.warning(f"Não foi possível carregar o dataset {dataset_name} armazenados na máquina local")
+        logger.info(f"Iniciando download do dataset {dataset_name}")
+
+        X_train, y_train = load_classification(dataset_name, split="Train")
+        X_test, y_test = load_classification(dataset_name, split="Test")
+
+        logger.info("Download finalizado com sucesso")
+
+        return {
+            "X_train": X_train,
+            "y_train": y_train,
+            "X_test": X_test,
+            "y_test": y_test,
+        }
