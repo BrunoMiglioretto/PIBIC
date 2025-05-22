cffce703a7b6a6997bcd104fa8d37052d8be70a6
Bruno Miglioretto <brunomiglioretto@gmail.com>
2025-05-22 07:54:04 -0300

    draft commit

--- utils/utils.py
+++ utils/utils.py
@@ -98,29 +98,29 @@ def transform_series(series, representation):
     return im_final
 
 
-def dimensions_fusion(img_dataset, operation):
-    """
-    operation: sum, subtraction, dot_product, element_wise
-    """
-
-    new_data = []
-    for dataset in img_dataset:
-        imgs = dataset.copy()
-        img_final = imgs.pop()
-        for img in imgs:
-            if operation == 'sum':
-                img_final += img
-            elif operation == 'subtraction':
-                img_final -= img
-            elif operation == 'dot_product':
-                img_final = np.dot(img_final, img)
-            elif operation == 'element_wise':
-                img_final = np.multiply(img_final, img)
-
-        flatten_img = img_final.flatten()
-        new_data.append(flatten_img)
-
-    return np.array(new_data)
+# def dimensions_fusion(img_dataset, operation):
+#     """
+#     operation: sum, subtraction, dot_product, element_wise
+#     """
+#
+#     new_data = []
+#     for dataset in img_dataset:
+#         imgs = dataset.copy()
+#         img_final = imgs.pop()
+#         for img in imgs:
+#             if operation == 'sum':
+#                 img_final += img
+#             elif operation == 'subtraction':
+#                 img_final -= img
+#             elif operation == 'dot_product':
+#                 img_final = np.dot(img_final, img)
+#             elif operation == 'element_wise':
+#                 img_final = np.multiply(img_final, img)
+#
+#         flatten_img = img_final.flatten()
+#         new_data.append(flatten_img)
+#
+#     return np.array(new_data)
 
 
 def PAA(s, w):
@@ -134,3 +134,48 @@ def PAA(s, w):
         res[i] = np.mean(s[start_idx:end_idx])
 
     return res
+
+
+def dimensions_fusion(img_dataset, operation):
+    """
+    operation: sum, subtraction, dot_product, element_wise
+    """
+    import numpy as np
+
+    new_data = []
+    for dataset in img_dataset:
+        imgs = dataset.copy()
+
+        # identificamos a maior dimensão dentro do dataset
+        max_shape = np.max(np.array([img.shape for img in imgs]), axis=0)
+
+        padded_imgs = []
+        for img in imgs:
+            # calculamos o padding necessário de acordo com a maior dimensão
+            pad_width = [(0, max_d - s) for max_d, s in zip(max_shape, img.shape)]
+            # aplicamos o padding adicionando zeros
+            padded_img = np.pad(img, pad_width, mode='constant', constant_values=0)
+            padded_imgs.append(padded_img)
+
+        img_final = padded_imgs[0]
+        for img in padded_imgs[1:]:
+            if operation == 'sum':
+                img_final += img
+            elif operation == 'subtraction':
+                img_final -= img
+            elif operation == 'dot_product':
+                # se todas as matrizes forem da mesma dimensão (ex. 5x5), podemos multiplicar diretamente
+                if img_final.shape[-1] == img.shape[0]:
+                    img_final = np.dot(img_final, img)
+                else:
+                    # tentativa de fallback (ns se vai funcionar, estou um pouco perdido nas propriedades do dot product)
+                    # basicamente, a ideia é que quando as dimensões não forem compatíveis para o dot product, podemos tentar pegar a matriz transposta
+                    # por ex, se img_final é 3x4 e a img é 2x4, podemos tentar fazer o dot product com a transposta da img, o que vai resultar na operação 3x4 @ 4x2 = 3x2
+                    img_final = np.dot(img_final, img.T)
+            elif operation == 'element_wise':
+                img_final = np.multiply(img_final, img)
+
+        flatten_img = img_final.flatten()
+        new_data.append(flatten_img)
+
+    return np.array(new_data)
