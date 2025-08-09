0914027d528a4b6022eb2ce1c9fc16cbac26ccc2
Bruno Miglioretto <brunomiglioretto@gmail.com>
2025-08-09 18:23:57 -0300

    big commit

--- utils/config.py
+++ utils/config.py
@@ -1,30 +1,20 @@
-import json
 import logging
 import time
+import os
 
-CONFIG_FILE = "/Users/brunomiglioretto/puc/iniciacao/pibic/config.json"
-
-RESULTS_FILENAME = f'results_final.csv'
-
-representations = ['RP', 'MTF', 'GASF', 'GADF', 'FIRTS', 'CWT']
-operations = ["sum", "subtraction", "dot_product", "element_wise"]
+from dotenv import load_dotenv
 
 
-with open(CONFIG_FILE) as f:
-    content = f.read()
-    config = json.loads(content)
+load_dotenv()
 
-DATASETS_FOLDER = config.get("datasets_folder")
-RESULTS_FOLDER = config.get("results_folder")
-LOGS_FOLDER = config.get("logs_folder")
-SKIP_DATASETS = config.get("skip_datasets")
-
+DATASETS_FOLDER = os.getenv("datasets_folder", default="datasets/data")
+RESULTS_FOLDER = os.getenv("results_folder", default="results/")
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     format='%(asctime)s %(levelname)s %(message)s',
     datefmt='%m/%d/%Y %I:%M:%S %p',
-    filename=f"{LOGS_FOLDER}/run-{time.time()}.txt",
+    filename=f"logs/run-{time.time()}.txt",
     filemode='w',
     level=logging.INFO
 )
--- utils/utils.py
+++ utils/utils.py
@@ -20,13 +20,6 @@ def load_dataset(dataset_name, dataset_folder):
         X_test, y_test = load_from_ts_file(f"{dataset_folder}/{dataset_name}/{dataset_name}_TEST.ts")
 
         logger.info("Carregamento finalizado com sucesso")
-
-        return {
-            "X_train": X_train,
-            "y_train": y_train,
-            "X_test": X_test,
-            "y_test": y_test,
-        }
     except Exception as e:
         logger.warning(e)
         logger.warning(f"Não foi possível carregar o dataset {dataset_name} armazenados na máquina local")
@@ -37,12 +30,12 @@ def load_dataset(dataset_name, dataset_folder):
 
         logger.info("Download finalizado com sucesso")
 
-        return {
-            "X_train": X_train,
-            "y_train": y_train,
-            "X_test": X_test,
-            "y_test": y_test,
-        }
+    return {
+        "X_train": X_train,
+        "y_train": y_train,
+        "X_test": X_test,
+        "y_test": y_test,
+    }
 
 
 def znorm(x):
@@ -50,6 +43,9 @@ def znorm(x):
     função para normalizar as séries na mesma escala
     a série ficará com uma média próxima de 0 e desvio-padrão próximo de 1
     """
+
+    if not np.any(x):
+        return x
 
     x_znorm = (x - np.mean(x)) / np.std(x)
     return x_znorm
@@ -62,104 +58,65 @@ def transform_series(series, representation):
     referência para entender um pouco melhor: https://pyts.readthedocs.io/en/stable/modules/image.html
     """
 
-    try:
-        series = np.array(znorm(series))
-        if representation == "CWT":
-            try:
-                coeffs, freqs = pywt.cwt(series, scales=np.arange(1, len(series) + 1), wavelet='morl') # morl
-                im_final = coeffs
-            except Exception as e:
-                logger.error(f"Problema ao transformar uma série em CWT: {e}")
-        elif representation == "MTF":
-            try:
-                series = series.reshape(1, len(series))
-                mtf = MarkovTransitionField(strategy='normal') #n_bins=4, strategy='uniform'
-                X_mtf = mtf.fit_transform(series)
-                im_final = X_mtf[0]
-            except Exception as e:
-                logger.error(f"Problema ao transformar uma série em MTF: {e}")
-        elif representation == "GADF":
-            try:
-                series = series.reshape(1, len(series))
-                gaf = GramianAngularField(method='difference')
-                X_gaf = gaf.fit_transform(series)
-                im_final = X_gaf[0]
-            except Exception as e:
-                logger.error(f"Problema ao transformar uma série em GADF: {e}")
-        elif representation == "GASF":
-            try:
-                series = series.reshape(1, len(series))
-                gaf = GramianAngularField(method='summation')
-                X_gaf = gaf.fit_transform(series)
-                im_final = X_gaf[0]
-            except Exception as e:
-                logger.error(f"Problema ao transformar uma série em GASF: {e}")
-        elif representation == "RP":
-            try:
-                series = series.reshape(1, len(series))
-                rp = RecurrencePlot(threshold='distance')
-                X_rp = rp.fit_transform(series)
-                im_final = X_rp[0]
-            except Exception as e:
-                logger.error(f"Problema ao transformar uma série em RP: {e}")
-        elif representation == "FIRTS":
-            try:
-                series = series.reshape(1, len(series))
-                mtf = MarkovTransitionField(n_bins=4, strategy='uniform')
-                X_mtf = mtf.fit_transform(series)
-                gaf = GramianAngularField(method='difference')
-                X_gaf = gaf.fit_transform(series)
-                rp = RecurrencePlot(threshold='distance')
-                X_rp = rp.fit_transform(series)
-                im_final = (X_mtf[0] + X_gaf[0] + X_rp[0])
-            except Exception as e:
-                logger.error(f"Problema ao transformar uma série em FIRTS: {e}")
-        return im_final
-    except Exception as e:
-        logger.error(f"Problema ao transformar uma série com o znorm: {e}")
+    series = np.array(znorm(series))
+    if representation == "CWT":
+        coeffs, freqs = pywt.cwt(series, scales=np.arange(1, len(series) + 1), wavelet='morl') # morl
+        im_final = coeffs
+    elif representation == "MTF":
+        series = series.reshape(1, len(series))
+        mtf = MarkovTransitionField(strategy='normal') #n_bins=4, strategy='uniform'
+        X_mtf = mtf.fit_transform(series)
+        im_final = X_mtf[0]
+    elif representation == "GADF":
+        series = series.reshape(1, len(series))
+        gaf = GramianAngularField(method='difference')
+        X_gaf = gaf.fit_transform(series)
+        im_final = X_gaf[0]
+    elif representation == "GASF":
+        series = series.reshape(1, len(series))
+        gaf = GramianAngularField(method='summation')
+        X_gaf = gaf.fit_transform(series)
+        im_final = X_gaf[0]
+    elif representation == "RP":
+        series = series.reshape(1, len(series))
+        rp = RecurrencePlot(threshold='distance')
+        X_rp = rp.fit_transform(series)
+        im_final = X_rp[0]
+    elif representation == "FIRTS":
+        series = series.reshape(1, len(series))
+        mtf = MarkovTransitionField(n_bins=4, strategy='uniform')
+        X_mtf = mtf.fit_transform(series)
+        gaf = GramianAngularField(method='difference')
+        X_gaf = gaf.fit_transform(series)
+        rp = RecurrencePlot(threshold='distance')
+        X_rp = rp.fit_transform(series)
+        im_final = (X_mtf[0] + X_gaf[0] + X_rp[0])
+    return im_final
 
 
 def dimensions_fusion(img_dataset, operation):
     """
     operation: sum, subtraction, dot_product, element_wise
     """
-    try:
-        new_data = []
-        for dataset in img_dataset:
-            imgs = dataset.copy()
-            img_final = imgs.pop()
-            for img in imgs:
-                if operation == 'sum':
-                    try:
-                        img_final += img
-                    except Exception as e:
-                        logger.error(f"Problema ao fundir usando sum: {e}")
-                elif operation == 'subtraction':
-                    try:
-                        img_final -= img
-                    except Exception as e:
-                        logger.error(f"Problema ao fundir usando subtraction: {e}")
-                elif operation == 'dot_product':
-                    try:
-                        img_final = np.dot(img_final, img)
-                    except Exception as e:
-                        logger.error(f"Problema ao fundir usando dot_product: {e}")
-                elif operation == 'element_wise':
-                    try:
-                        img_final = np.multiply(img_final, img)
-                    except Exception as e:
-                        logger.error(f"Problema ao fundir usando element_wise: {e}")
 
-            try:
-                flatten_img = img_final.flatten()
-                new_data.append(flatten_img)
+    new_data = []
+    for dataset in img_dataset:
+        imgs = dataset.copy()
+        img_final = imgs.pop()
+        for img in imgs:
+            if operation == 'sum':
+                img_final += img
+            elif operation == 'subtraction':
+                img_final -= img
+            elif operation == 'dot_product':
+                img_final = np.dot(img_final, img)
+            elif operation == 'element_wise':
+                img_final = np.multiply(img_final, img)
 
-                new_series = np.array(new_data)
-            except Exception as e:
-                logger.error(f"Problema ao transformar as imagens em um numpy array: {e}")
-            return new_series
-    except Exception as e:
-        logger.error(f"Problema geral ao fundir as séries: {e}")
+        flatten_img = img_final.flatten()
+        new_data.append(znorm(flatten_img))
+
+    return np.array(new_data)
 
 
 def PAA(s, w):
